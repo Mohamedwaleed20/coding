5775604f7724cd634ea14b8db2f0d49a852a3010
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-19 11:33:59 +0300

    Add project files.

---
+++ coding/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("what are you doing ? ");
+string doing = Console.ReadLine();
+Console.WriteLine("what are you drinking ? ");
+string drink = Console.ReadLine();
+Console.WriteLine("do you love coding or networking ? ");
+string love = Console.ReadLine();
+if (love == "coding")
+{
+    Console.WriteLine($"""
+I'm a programmer 
+I write code
+I don't fix computers
+""");
+}
+else if (love == "networking")
+{
+    Console.WriteLine("5G");
+}
+else
+{
+    Console.WriteLine("error");
+}
+int brain = int.Parse(Console.ReadLine());
+if (brain == 0)
+{
+    Console.WriteLine("order Coffee()");
+}
+else
+{
+    Console.WriteLine("keep Coding()");
+}
+Console.ReadLine();
